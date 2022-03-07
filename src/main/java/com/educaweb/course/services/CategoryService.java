af8e02551fb31b7a45bfbfceb32ef06b19e81943
livia.cordeiro <livia.cordeiro@tecnogroup.com.br>
2022-03-07 17:27:59 -0300

    Category

---
+++ src/main/java/com/educaweb/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.educaweb.course.services;
+
+import com.educaweb.course.entities.Category;
+import com.educaweb.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = categoryRepository.findById(id);
+        return obj.get();
+    }
+}
